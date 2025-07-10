7c1cf798b5ebdabd42f6d9f96a9de4d03a9a8700
Gerard Marull-Paretas <gerard@teslabs.com>
2025-07-10 13:29:28 +0200

    libc: extend math library with some floating point functions
    Added functions are needed to use nRF Fuel Gauge library as it operates
    using single-precision floating point. Code has been adapted from Nuttx
    as it has a complete libc with a permissive license.
    
    At some point we should use a proper libc (picolibc, newlib, ... ?),
    src/libc is just a joke.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- src/libc/include/math.h
+++ src/libc/include/math.h
@@ -1,5 +1,6 @@
 /*
  * Copyright 2024 Google LLC
+ * Copyright 2025 Apache Software Foundation (ASF)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,8 +17,36 @@
 
 #pragma once
 
+#ifndef _HUGE_ENUF
+#  define _HUGE_ENUF (1e+300)  /* _HUGE_ENUF*_HUGE_ENUF must overflow */
+#endif
+
+#define INFINITY   ((double)(_HUGE_ENUF * _HUGE_ENUF))
+
+#define INFINITY_F ((float)INFINITY)
+#define NAN_F      ((float)(INFINITY * 0.0F))
+
+#define M_E        2.7182818284590452353602874713526625
+
 typedef float float_t;
 
 typedef double double_t;
+
+#define isinff(x)  (((x) == INFINITY_F) || ((x) == -INFINITY_F))
+#define isnanf(x)  ((x) != (x))
+
+float expf(float x);
+
+float fabsf(float x);
+
+float floorf(float x);
+
+float fmaxf(float x, float y);
+
+float fminf(float x, float y);
+
+float logf(float x);
+
+float modff(float x, float *iptr);
 
 double round(double d);
