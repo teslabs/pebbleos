24fe31d19c0aef1f32058bfccd87a78945ff79ff
Joshua Wise <joshua@accelerated.tech>
2025-07-17 13:57:43 -0700

    bluetooth_fw: add workaround for FIRM-233 that cycles airplane mode on affected devices
    Signed-off-by: Joshua Wise <joshua@accelerated.tech>

--- platform/platform_capabilities.py
+++ platform/platform_capabilities.py
@@ -56,6 +56,7 @@ master_capability_set = {
     'USE_PARALLEL_FLASH',
     'HAS_PUTBYTES_PREACKING',
     'HAS_FLASH_OTP',
+    'NEEDS_FIRM_233_WAR',
 }
 
 board_capability_dicts = [
@@ -281,6 +282,7 @@ board_capability_dicts = [
             'HAS_VIBE_DRV2604',
             'HAS_PMIC',
             'HAS_FLASH_OTP',
+            'NEEDS_FIRM_233_WAR',
         },
     },
     {
