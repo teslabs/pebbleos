eb951d48a5595a85492cd824d514768edfc1708c
Matthew Wardrop <mpwardrop@gmail.com>
2025-08-01 23:17:21 -0700

    fw/drivers/imu/asterix: Leverage new lsm6dso driver
    Signed-off-by: Matthew Wardrop <mpwardrop@gmail.com>

--- src/fw/drivers/imu/imu_asterix.c
+++ src/fw/drivers/imu/imu_asterix.c
@@ -1,10 +1,11 @@
 #include "board/board.h"
-
 #include "drivers/accel.h"
+#include "drivers/i2c.h"
 #include "drivers/imu.h"
+#include "drivers/imu/lsm6dso/lsm6dso.h"
 #include "drivers/spi.h"
 #include "kernel/util/delay.h"
-#include "drivers/i2c.h"
+#include "system/logging.h"
 
 #define MMC5603_PRODUCT_ID 0x39
 #define MMC5603_PRODUCT_ID_VALUE 0x10
@@ -13,17 +14,6 @@
 #define BMP390_CHIP_ID 0x00
 #define BMP390_CHIP_ID_VALUE 0x60
 #define BMP390_PWR_CTRL 0x1B
-
-#define LSM6D_FUNC_CFG_ACCESS 0x01
-
-#define LSM6D_WHO_AM_I 0x0F
-#define LSM6D_WHO_AM_I_VALUE 0x6C
-
-#define LSM6D_CTRL1_XL 0x10
-#define LSM6D_CTRL2_G 0x11
-#define LSM6D_CTRL4_C 0x13
-#define LSM6D_CTRL4_C_SLEEP_G 0x40
-
 
 
 static bool prv_read_register(I2CSlavePort *i2c, uint8_t register_address, uint8_t *result) {
@@ -63,23 +53,13 @@ void imu_init(void) {
     (void) prv_write_register(I2C_BMP390, BMP390_PWR_CTRL, 0);
   }
   
-  rv = prv_read_register(I2C_LSM6D, LSM6D_WHO_AM_I, &result);
-  if (!rv || result != LSM6D_WHO_AM_I_VALUE) {
-    PBL_LOG(LOG_LEVEL_DEBUG, "LSM6DSO probe failed; rv %d, result 0x%02x", rv, result);
-  } else {
-    PBL_LOG(LOG_LEVEL_DEBUG, "found the LSM6DSO, setting to lower power");
-    (void) prv_write_register(I2C_LSM6D, LSM6D_FUNC_CFG_ACCESS, 0);
-    (void) prv_write_register(I2C_LSM6D, LSM6D_CTRL1_XL, 0);
-    (void) prv_write_register(I2C_LSM6D, LSM6D_CTRL2_G, 0);
-    (void) prv_write_register(I2C_LSM6D, LSM6D_CTRL4_C, LSM6D_CTRL4_C_SLEEP_G);
-  }
+  lsm6dso_init();
 }
 
 void imu_power_up(void) {
-  // NYI
+  lsm6dso_power_up();
 }
 
 void imu_power_down(void) {
-  // NYI
+  lsm6dso_power_down();
 }
-
