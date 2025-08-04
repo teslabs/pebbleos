2388831ce408dc1e3b72f6923b3d1c14c620489f
Gerard Marull-Paretas <gerard@teslabs.com>
2025-08-04 15:59:18 +0200

    bluetooth-fw: add bt_driver_handle_host_added|removed_cccd stubs
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- src/bluetooth-fw/qemu/bonding_sync.c
+++ src/bluetooth-fw/qemu/bonding_sync.c
@@ -22,3 +22,9 @@ void bt_driver_handle_host_added_bonding(const BleBonding *bonding) {
 
 void bt_driver_handle_host_removed_bonding(const BleBonding *bonding) {
 }
+
+void bt_driver_handle_host_added_cccd(const BleCCCD *cccd) {
+}
+
+void bt_driver_handle_host_removed_cccd(const BleCCCD *cccd) {
+}
