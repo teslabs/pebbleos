c1411245a77659f35803b30cc82ff45cd988bdb5
Gerard Marull-Paretas <gerard@teslabs.com>
2025-08-08 12:35:45 +0200

    bluetooth-fw/nimble: minor tweaks to bt_driver_le_connection_parameter_update
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- src/bluetooth-fw/nimble/responsiveness.c
+++ src/bluetooth-fw/nimble/responsiveness.c
@@ -23,11 +23,12 @@ bool bt_driver_le_connection_parameter_update(const BTDeviceInternal *addr,
                                               const BleConnectionParamsUpdateReq *req) {
   ble_addr_t nimble_addr;
   struct ble_gap_conn_desc desc;
-  struct ble_gap_upd_params params = { 0 };
+  struct ble_gap_upd_params params;
+  int rc;
 
   pebble_device_to_nimble_addr(addr, &nimble_addr);
 
-  int rc = ble_gap_conn_find_by_addr(&nimble_addr, &desc);
+  rc = ble_gap_conn_find_by_addr(&nimble_addr, &desc);
   if (rc != 0) {
     PBL_LOG(LOG_LEVEL_ERROR, "ble_gap_conn_find_by_addr failed: %d", rc);
     return false;
@@ -35,15 +36,17 @@ bool bt_driver_le_connection_parameter_update(const BTDeviceInternal *addr,
 
   pebble_conn_update_to_nimble(req, &params);
 
-  PBL_LOG(LOG_LEVEL_DEBUG, "Requesting connection parameter update: "
-          "interval=(%u, %u), latency=%u, timeout=%u, ce_len=(%u, %u)",
-          params.itvl_min, params.itvl_max, params.latency, params.supervision_timeout,
-          params.min_ce_len, params.max_ce_len);
+  PBL_LOG(LOG_LEVEL_DEBUG,
+          "Request connection parameters: "
+          "interval=(%u, %u) ms, latency=%u, spvn timeout=%u ms",
+          params.itvl_min * BLE_HCI_CONN_ITVL / 1000, params.itvl_max * BLE_HCI_CONN_ITVL / 1000,
+          params.latency, params.supervision_timeout * BLE_HCI_CONN_SPVN_TMO_UNITS);
 
   rc = ble_gap_update_params(desc.conn_handle, &params);
   if (rc != 0) {
     PBL_LOG(LOG_LEVEL_ERROR, "ble_gap_update_params failed: 0x%04x", (uint16_t)rc);
     return false;
   }
+
   return true;
 }
