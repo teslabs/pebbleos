5849b5e72152f8a5f028f05512cb800b6098e8d5
Gerard Marull-Paretas <gerard@teslabs.com>
2025-09-09 09:30:32 +0200

    resources/normal/base/tzdata: port update script to Python 3
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- resources/normal/base/tzdata/update_timezones.py
+++ resources/normal/base/tzdata/update_timezones.py
@@ -16,22 +16,36 @@
 
 import os
 import tempfile
-import shutil
 
 import sh
+
+TZDATA_DIR = os.path.dirname(os.path.abspath(__file__))
 
 tmp_dir = tempfile.mkdtemp()
 os.chdir(tmp_dir)
 
-print 'Downloading timezone data to {}'.format(tmp_dir)
+print("Downloading timezone data to {}".format(tmp_dir))
 
-sh.wget('--retr-symlinks', 'ftp://ftp.iana.org/tz/tz*-latest.tar.gz')
+sh.wget("https://ftp.iana.org/tz/tzdata-latest.tar.gz")
 
-print 'Download complete'
-print sh.ls('-la', 'tzdata-latest.tar.gz').strip()
+print("Download complete")
+print(sh.ls("-la", "tzdata-latest.tar.gz").strip())
 
-sh.tar('-xvzf', 'tzdata-latest.tar.gz')
+sh.tar("-xvzf", "tzdata-latest.tar.gz")
+
+tz_file = os.path.join(TZDATA_DIR, "timezones_olson.txt")
 
 # backward goes last so we can just always do backreferences for links
-sh.cat('africa', 'antarctica', 'asia', 'australasia', 'europe', 'etcetera', 'northamerica', 'southamerica', 'backward', _out='timezones_olson.txt')
-print 'Updated database written to {}'.format(os.path.realpath('timezones_olson.txt'))
+sh.cat(
+    "africa",
+    "antarctica",
+    "asia",
+    "australasia",
+    "europe",
+    "etcetera",
+    "northamerica",
+    "southamerica",
+    "backward",
+    _out=tz_file,
+)
+print("Updated database written to {}".format(tz_file))
