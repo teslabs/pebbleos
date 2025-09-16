f7078f956f28d9fba706a9663f8f947df0990417
Joshua Jun <joshuajun@proton.me>
2025-09-16 11:48:56 +0200

    Revert "fw/third_party/speex: update memory allocation to use task-level functions"
    This reverts commit da916cf705a0a4949ae9461ba38e9db25fd8798a.
    
    Signed-off-by: Joshua Jun <joshuajun@proton.me>

--- third_party/speex/os_support_custom.h
+++ third_party/speex/os_support_custom.h
@@ -5,17 +5,16 @@
 #include <stdint.h>
 #include <stddef.h>
 
-/* Use task-level allocation so Speex temporary/scratch buffers are
-	allocated on the current task/app heap instead of the kernel heap. */
-extern void *task_zalloc_check(size_t size);
-extern void *task_realloc(void *ptr, size_t size);
-extern void task_free(void *ptr);
+/* Forward declare PebbleOS functions */
+extern void *kernel_zalloc_check(size_t size);
+extern void *kernel_realloc(void *ptr, size_t size);
+extern void kernel_free(void *ptr);
 
-/* Override memory allocation functions to use the task/app heap */
-static inline void *speex_alloc(int size) { return task_zalloc_check((size_t)size); }
-static inline void *speex_alloc_scratch(int size) { return task_zalloc_check((size_t)size); }
-static inline void *speex_realloc(void *ptr, int size) { return task_realloc(ptr, (size_t)size); }
-static inline void speex_free(void *ptr) { task_free(ptr); }
+/* Override memory allocation functions to use PebbleOS kernel functions */
+static inline void *speex_alloc(int size) { return kernel_zalloc_check(size); }
+static inline void *speex_alloc_scratch(int size) { return kernel_zalloc_check(size); }
+static inline void *speex_realloc(void *ptr, int size) { return kernel_realloc(ptr, size); }
+static inline void speex_free(void *ptr) { kernel_free(ptr); }
 
 /* Override debug/warning functions to be no-ops for embedded system */
 static inline void _speex_fatal(const char *str, const char *file, int line) { (void)str; (void)file; (void)line; }
