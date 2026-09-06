fc0a216e048f1587b0ab3f6d775dbdc23086176b
Nick Schonning <nschonni@gmail.com>
2026-09-06 18:56:32 -0400

    src/fw/kernel: fix PebbleAppCacheEventNum typo
    Signed-off-by: Nick Schonning <nschonni@gmail.com>

--- src/fw/kernel/events.h
+++ src/fw/kernel/events.h
@@ -708,7 +708,7 @@ _Static_assert(sizeof(PebbleTimelinePeekEvent) == 8,
 typedef enum PebbleAppCacheEventType {
   PebbleAppCacheEvent_Removed,
 
-  PebbleAppCacehEventNum
+  PebbleAppCacheEventNum
 } PebbleAppCacheEventType;
 
 typedef struct PACKED PebbleAppCacheEvent {
