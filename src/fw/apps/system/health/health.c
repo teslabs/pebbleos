cd112d68189bb2aecc7cd9e6bc46cd0977555fe3
Nick Schonning <nschonni@gmail.com>
2026-09-07 12:15:48 -0400

    fw/apps/system/health: fix prv_finish_initialization_cb typo
    Signed-off-by: Nick Schonning <nschonni@gmail.com>

--- src/fw/apps/system/health/health.c
+++ src/fw/apps/system/health/health.c
@@ -89,7 +89,7 @@ static void prv_show_insights_onboarding_dialog(void) {
 }
 
 //! Initialize application
-static void prv_finish_initilization_cb(bool in_focus) {
+static void prv_finish_initialization_cb(bool in_focus) {
   if (in_focus) {
     HealthAppData *health_app_data = app_state_get_user_data();
 
@@ -146,7 +146,7 @@ static void prv_initialize(void) {
 
   // Finish up initializing the app a bit later. This helps reduce lag when opening the app
   app_focus_service_subscribe_handlers((AppFocusHandlers){
-    .did_focus = prv_finish_initilization_cb,
+    .did_focus = prv_finish_initialization_cb,
   });
 }
 
